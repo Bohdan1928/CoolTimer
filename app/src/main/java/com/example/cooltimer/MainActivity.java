8f1e66f662eeea909117080707d30c5df4bca5f7
PC <bbigun2000@gmail.com>
2021-11-17 14:58:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cooltimer/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.cooltimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView textView;
+    private Button button;
+    private boolean isTimerOn;
+    private CountDownTimer countDownTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        isTimerOn = false;
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        initializeSeekBar();
+        initializeButtonStartStop();
+    }
+
+    public void initializeButtonStartStop() {
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                createTimer();
+                button.setText("STOP");
+            }
+        });
+    }
+
+    public void updateTimer(long millisUtilFinished) {
+        int minutes = (int) millisUtilFinished / 1000 / 60;
+        int seconds = (int) millisUtilFinished / 1000 - (minutes * 60);
+
+        String minute = "";
+        String second = "";
+
+        if (minutes < 10) {
+            minute = "0" + minutes;
+        } else {
+            minute = String.valueOf(minutes);
+        }
+
+        if (seconds < 10) {
+            second = "0" + seconds;
+        } else {
+            second = String.valueOf(seconds);
+        }
+
+        textView.setText(minute + ":" + second);
+    }
+
+    public void createTimer() {
+        if (!isTimerOn){
+            seekBar.setEnabled(false);
+            isTimerOn = true;
+
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000L, 1000) {
+                @Override
+                public void onTick(long l) {
+                    updateTimer(l);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bell_sound);
+                    mediaPlayer.start();
+                    resetTimer();
+                }
+            }.start();
+
+        } else{
+            resetTimer();
+        }
+    }
+
+
+    public void initializeSeekBar() {
+        seekBar.setMax(600);
+        seekBar.setProgress(60);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                long progressInMillis = i * 1000L;
+                updateTimer(progressInMillis);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void resetTimer(){
+        countDownTimer.cancel();
+        isTimerOn = false;
+        textView.setText("00:59");
+        button.setText("START");
+        seekBar.setEnabled(true);
+        seekBar.setProgress(59);
+    }
+}
+
